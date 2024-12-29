397e17873be0da1ffd6c8bbb451095bbe4ba9cd6
Danish Awasthi <awasthidanish2003@gmail.com>
2024-12-30 00:25:13 +0530

    Time: 1 ms (100.00%) | Memory: 133.9 MB (46.84%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        if(nums.length<3) return false;
+        int n1=Integer.MAX_VALUE;
+        int n2=Integer.MAX_VALUE;
+        for(int num:nums){
+            if(num<=n1) n1=num;
+            else if(num<=n2) n2=num;
+            else return true;
+        }
+        return false;
+    }
+}
