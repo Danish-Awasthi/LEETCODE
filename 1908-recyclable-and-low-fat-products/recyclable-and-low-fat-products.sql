5da6650de9d1250b40cdaced42ceabb96b5e91da
Danish Awasthi <awasthidanish2003@gmail.com>
2025-02-02 08:53:18 +0530

    Time: 1161 ms (21.48%) | Memory: 0B (100.00%) - LeetSync

--- 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -1,3 +1,2 @@
 # Write your MySQL query statement below
-Select product_id from Products
-where low_fats='Y' AND recyclable='Y';
+Select product_id from Products where low_fats='Y' AND recyclable='Y';
